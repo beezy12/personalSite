08dcacda61440a6c2e8b302e3265d271b6d4a86d
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-03-01 15:03:46 -0600

    got them split, back to animations

--- js/stuff.js
+++ js/stuff.js
@@ -9,41 +9,69 @@ $(document).ready(function() {
     //     $hi.css({'visibility': 'hidden'});
     // }, 6000);
 
-    $("#aboutMe2").hide();
+
+
+
+    var $aboutMe = $('#aboutMe');
+    var $aboutMe2 = $('#aboutMe2');
+    var $aboutMe3 = $('#aboutMe3');
+
+    $aboutMe2.hide();
+    $aboutMe3.hide();
 
     function hide1() {
-        $("#aboutMe").fadeIn(1000);
-        $("#aboutMe").fadeOut(1000, hide2);
+
+        $aboutMe.delay('slow').fadeIn(1000);
+        $aboutMe.fadeOut(2000, hide2);
     }
+
+
 
     function hide2() {
 
-        $("#aboutMe2").fadeIn(1000);
-        $("#aboutMe2").fadeOut(1000, hide1);
+        $aboutMe2.fadeIn(1000);
+        $aboutMe2.fadeOut(2000, hide3);
     }
+
+    function hide3() {
+
+        $aboutMe3.fadeIn(1000);
+        // $aboutMe3.fadeOut(2000);
+
+    }
+
     hide1();
 
-    $aboutMe = $('.aboutMe');
-    $aboutMe.hide();
-    var timeOut = setTimeout(function() {
-        $aboutMe.fadeIn();
-    }, 2000);
-
-    var timeOut = setTimeout(function() {
-        $aboutMe.css({'visibility': 'none'});
-        $aboutMe.fadeOut('slow');
-    }, 4000);
-
-    $aboutMe2 = $('.aboutMe2');
-    $aboutMe2.hide();
-    var timeOut = setTimeout(function() {
-        $aboutMe2.fadeIn();
-    }, 5000);
 
 
-    var timeOut = setTimeout(function() {
-        $aboutMe.css({'display': 'none'});
-    }, 6000);
+
+
+
+
+
+
+
+    // $aboutMe = $('.aboutMe');
+    // $aboutMe.hide();
+    // var timeOut = setTimeout(function() {
+    //     $aboutMe.fadeIn();
+    // }, 2000);
+
+    // var timeOut = setTimeout(function() {
+    //     $aboutMe.css({'visibility': 'none'});
+    //     $aboutMe.fadeOut('slow');
+    // }, 4000);
+
+    // $aboutMe2 = $('.aboutMe2');
+    // $aboutMe2.hide();
+    // var timeOut = setTimeout(function() {
+    //     $aboutMe2.fadeIn();
+    // }, 5000);
+
+
+    // var timeOut = setTimeout(function() {
+    //     $aboutMe.css({'display': 'none'});
+    // }, 6000);
 
 
     // $first = $('#firstUrl');
