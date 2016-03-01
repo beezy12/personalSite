d8ba40fcdd0eab7f44b19f5bf441b4199e71ba40
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-03-01 13:23:34 -0600

    making a splitting branch. animations are not done

--- js/stuff.js
+++ js/stuff.js
@@ -1,51 +1,75 @@
-;$(document).ready(function() {
+$(document).ready(function() {
 
 
 
 
 
-    $hi = $('.hi');
-    var timeOut = setTimeout(function() {
-        // $hi.fadeOut();
-        $hi.css({'visibility': 'hidden'});
-    }, 10000);
+    // $hi = $('.hi');
+    // var timeOut = setTimeout(function() {
+    //     $hi.css({'visibility': 'hidden'});
+    // }, 6000);
+
+    // $("#aboutMe2").hide();
+
+    // function hide1() {
+    //     $("#aboutMe").fadeIn(1000);
+    //     $("#aboutMe").fadeOut(1000, hide2);
+    // }
+
+    // function hide2() {
+
+    //     $("#aboutMe2").fadeIn(1000);
+    //     $("#aboutMe2").fadeOut(1000, hide1);
+    // }
+    // hide1();
 
     $aboutMe = $('.aboutMe');
     $aboutMe.hide();
     var timeOut = setTimeout(function() {
         $aboutMe.fadeIn();
-    }, 3000);
-
-    $myProfile = $('.myProfile');
-    $myProfile.hide();
-    var timeOut = setTimeout(function() {
-        $myProfile.fadeIn();
     }, 2000);
 
-    $first = $('#firstUrl');
-    $second = $('#secondUrl');
-    $button = $('.start');
-    $first.hide();
-    $second.hide();
-    $button.hide();
     var timeOut = setTimeout(function() {
-        $first.fadeIn();
-        $second.fadeIn();
-        $button.fadeIn();
-    }, 10000);
+        $aboutMe.css({'visibility': 'none'});
+        $aboutMe.fadeOut('slow');
+    }, 4000);
+
+    $aboutMe2 = $('.aboutMe2');
+    $aboutMe2.hide();
+    var timeOut = setTimeout(function() {
+        $aboutMe2.fadeIn();
+    }, 5000);
 
 
-    $(function() {
-    $('#baseUrl').click( function() {
-        var furl = $('#firstUrl').val();
-        var surl = $('#secondUrl').val();
-        $('#firstUrl').val('');
-        $('#secondUrl').val('');
-        window.location = $(this).attr('href') + '/' + furl + '/' + surl;
-        return false;
-        });
+    var timeOut = setTimeout(function() {
+        $aboutMe.css({'display': 'none'});
+    }, 6000);
 
-    });
+
+    // $first = $('#firstUrl');
+    // $second = $('#secondUrl');
+    // $button = $('.start');
+    // $first.hide();
+    // $second.hide();
+    // $button.hide();
+    // var timeOut = setTimeout(function() {
+    //     $first.fadeIn();
+    //     $second.fadeIn();
+    //     $button.fadeIn();
+    // }, 10000);
+
+
+    // $(function() {
+    // $('#baseUrl').click( function() {
+    //     var furl = $('#firstUrl').val();
+    //     var surl = $('#secondUrl').val();
+    //     $('#firstUrl').val('');
+    //     $('#secondUrl').val('');
+    //     window.location = $(this).attr('href') + '/' + furl + '/' + surl;
+    //     return false;
+    //     });
+
+    // });
 
 
 
